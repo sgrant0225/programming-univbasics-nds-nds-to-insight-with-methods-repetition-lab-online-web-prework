e9506dabcdcbf37bf6a33b1f8626bbf952cd1475
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-31 19:00:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -36,6 +36,8 @@ list_of_directors
 end
 
 def total_gross(source)
+  
+end
   # Write this implementation
   #
   # Should use methods:
@@ -45,6 +47,6 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-end
+
 
 
